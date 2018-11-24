09b15f2897926886a6e8c701a3a8aa03ed546d3e
MAClavell <michaelclavell11@gmail.com>
2018-11-24 18:27:12 -0500

    Updated names and documentation

--- AI System/OM_Action_NearPlayer.cs
+++ AI System/OM_Action_NearPlayer.cs
@@ -6,17 +6,12 @@ public class OM_Action_NearPlayer : OM_Action
 {
 	//Fields
 	float timer;
-	float deathTotal = 0.75f;
+	float deathTotal = 0.75f; //the timer maximum until the old man dies
 
 	/// <summary>
-	/// Create an action where the Old Man stands in place
+	/// Create an action where the old man stands still until the player leave's his radius or a timer is reached
 	/// </summary>
-	/// <param name="priority">How important the action is</param>
-	/// <param name="name">The name of this action</param>
-	/// <param name="standLength">How long the stand lasts for, x lessthan/= 0 for infinite</param>
-	/// <param name="standPoint">The Vector3 to stand at. If null is passed in then there is no movement</param>
 	/// <param name="thoughtItem">The sprite to put in the thought bubble. Can be left null for no item to be shown</param>
-	/// <param name="discardTime">The amount of time this action can spend in the queue without being discarded. 0 for infinite.</param>
 	public OM_Action_NearPlayer(Sprite thoughtItem = null)
 		: base(0, "NearPlayer", OM_ActionType.NearPlayer, thoughtItem, 0)
 	{
@@ -46,10 +41,10 @@ public class OM_Action_NearPlayer : OM_Action
 
 		if (!GameManager.Instance.player.IsNearOldMan)
 		{
-            CompleteAction();
+			CompleteAction();
 			PuzzleManager.Instance.OM_inDanger = false;
 		}
-		if(timer >= deathTotal)
+		if (timer >= deathTotal)
 		{
 			GameManager.Instance.KillOldMan();
 		}
